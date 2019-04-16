2bc79058022fe40984078a827f0ba02ae2601d1f
Imane <imane@MacBook-Pro-de-Imane.local>
2019-04-16 18:31:19 +0200

    init

---
+++ my-app/src/pages/HomePage.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+
+class HomePage extends Component {
+
+    render() {
+        return (
+            <div>
+            <h1>Twister</h1>
+                    <Link to="/login">Login</Link> <Link to="/sign-up">Sign up</Link>
+            </div>)
+
+
+    }
+}
+
+export default HomePage;
+
+
+
